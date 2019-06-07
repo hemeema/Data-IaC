eeb972e732dce685a1c182e3aaa2eb9d33de50cc
Adam <adam.michalsky@amwater.com>
2019-06-07 14:57:04 -0400

    updated to pull raw content

--- aws-scripts/hdf/resources/worker-userdata.sh
+++ aws-scripts/hdf/resources/worker-userdata.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 sudo sed -i 's/hostname=localhost/hostname=hostname/g' /etc/ambari-agent/conf/ambari-agent.ini
-sudo wget https://github.com/adammichalsky/Data-IaC/blob/master/aws-scripts/hdf/resources/jars/ngdbc-2.1.2.jar
-sudo wget https://github.com/adammichalsky/Data-IaC/blob/master/aws-scripts/hdf/resources/jars/nifi-hana-custom-processor-2.0.jar
-sudo wget https://github.com/adammichalsky/Data-IaC/blob/master/aws-scripts/hdf/resources/scripts/nifi-connectors.path
-sudo wget https://github.com/adammichalsky/Data-IaC/blob/master/aws-scripts/hdf/resources/scripts/nifi-connectors.svc
-sudo wget https://github.com/adammichalsky/Data-IaC/blob/master/aws-scripts/hdf/resources/scripts/nifi-move-connectors.sh
-sudo wget https://github.com/adammichalsky/Data-IaC/blob/master/aws-scripts/hdf/resources/scripts/nifi-custom-processors.path
-sudo wget https://github.com/adammichalsky/Data-IaC/blob/master/aws-scripts/hdf/resources/scripts/nifi-custom-processors.svc
-sudo wget https://github.com/adammichalsky/Data-IaC/blob/master/aws-scripts/hdf/resources/scripts/nifi-custom-processors.sh
+sudo wget https://raw.githubusercontent.comadammichalsky/Data-IaC/blob/master/aws-scripts/hdf/resources/jars/ngdbc-2.1.2.jar
+sudo wget https://raw.githubusercontent.com/adammichalsky/Data-IaC/blob/master/aws-scripts/hdf/resources/jars/nifi-hana-custom-processor-2.0.jar
+sudo wget https://raw.githubusercontent.com/adammichalsky/Data-IaC/blob/master/aws-scripts/hdf/resources/scripts/nifi-connectors.path
+sudo wget https://raw.githubusercontent.com/adammichalsky/Data-IaC/blob/master/aws-scripts/hdf/resources/scripts/nifi-connectors.svc
+sudo wget https://raw.githubusercontent.com/adammichalsky/Data-IaC/blob/master/aws-scripts/hdf/resources/scripts/nifi-move-connectors.sh
+sudo wget https://raw.githubusercontent.com/adammichalsky/Data-IaC/blob/master/aws-scripts/hdf/resources/scripts/nifi-custom-processors.path
+sudo wget https://raw.githubusercontent.com/adammichalsky/Data-IaC/blob/master/aws-scripts/hdf/resources/scripts/nifi-custom-processors.svc
+sudo wget https://raw.githubusercontent.com/adammichalsky/Data-IaC/blob/master/aws-scripts/hdf/resources/scripts/nifi-custom-processors.sh
 mv /tmp/nifi-connectors.path /etc/systemd/system/nifi-connectors.path
 mv /tmp/nifi-connectors.svc /etc/systemd/system/nifi-connectors.service
 mv /tmp/nifi-custom-processors.path /etc/systemd/system/nifi-custom-processors.path
