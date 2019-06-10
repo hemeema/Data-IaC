7e75204adbf86ac9e55c769a6b73ae7b13fd8ba1
Adam <adam.michalsky@amwater.com>
2019-06-10 12:46:20 -0400

    Update create-msk-cluster.py

--- aws/msk/create-msk-cluster.py
+++ aws/msk/create-msk-cluster.py
@@ -4,9 +4,6 @@ import json
 import sys
 import os
 
-def get_session(region):
-    return boto3.session.Session(region_name=region)
-
 subnetA = sys.argv[1]
 subnetB = sys.argv[2]
 subnetC = sys.argv[3]
@@ -14,13 +11,13 @@ securityGroup = sys.argv[4]
 
 #Prepare template file for MSK cluster
 #Update subnetA in msk-template
-os.system("sudo sed -i 's/subnetA/" + subnetA + "/g' /tmp/msk-cluster.json")
+os.system("sudo sed -i 's/subnetA/" + subnetA + "/g' /tmp/aws/aws/msk/msk-cluster.json")
 #Update subnetB in msk-template
-os.system("sudo sed -i 's/subnetB/" + subnetB + "/g' /tmp/msk-cluster.json")
+os.system("sudo sed -i 's/subnetB/" + subnetB + "/g' /tmp/aws/aws/msk/msk-cluster.json")
 #Update subnetC in msk-template
-os.system("sudo sed -i 's/subnetC/" + subnetC + "/g' /tmp/msk-cluster.json")
+os.system("sudo sed -i 's/subnetC/" + subnetC + "/g' /tmp/aws/aws/msk/msk-cluster.json")
 #Update securityGroup in msk-template
-os.system("sudo sed -i 's/securityGroup/"+ securityGroup + "/g' /tmp/msk-cluster.json")
+os.system("sudo sed -i 's/securityGroup/"+ securityGroup + "/g' /tmp/aws/aws/msk/msk-cluster.json")
 
 
 # Create Session
