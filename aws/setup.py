80bca4408621333a2c02682d7226acd89c07784f
Adam <adam.michalsky@amwater.com>
2019-06-10 20:07:10 -0400

    added files

---
+++ aws/setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(
+    name='AW-AWS Big Data Utilities',
+    version='0.1dev',
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+)
